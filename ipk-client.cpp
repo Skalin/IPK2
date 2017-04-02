45b37302b9bdf00d7afb4aefb7a4bfeae58b720e
Dominik Skála <dominikskala@seznam.cz>
2017-04-02 14:00:33 +0200

    - removing warnings

--- ipk-client.cpp
+++ ipk-client.cpp
@@ -232,7 +232,7 @@ int main(int argc, char *argv[]) {
 
 	freeaddrinfo(res);
 
-	int client_socket;
+	unsigned int client_socket;
 	if (ip == 4) {
 		if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) <= 0) {
 			throwException("Error: Could not open client socket.");
